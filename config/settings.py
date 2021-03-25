88073450caef7c10283612830f75e827f42dfcab
Hayk Ghukasyan <76152476+GhukasyanHayk@users.noreply.github.com>
2021-03-25 17:38:53 +0400

    -

--- config/settings.py
+++ config/settings.py
@@ -115,6 +115,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [STATIC_DIR]
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "estelarestoran@gmail.com"
