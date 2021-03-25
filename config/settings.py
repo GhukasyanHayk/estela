ae1dc3c071e2aca68d42c00370c4ab0c1434eb5d
Hayk Ghukasyan <76152476+GhukasyanHayk@users.noreply.github.com>
2021-03-25 17:43:17 +0400

    -

--- config/settings.py
+++ config/settings.py
@@ -111,11 +111,13 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [STATIC_DIR]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "estelarestoran@gmail.com"
