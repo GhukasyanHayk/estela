400a7509450b0c7a5170b288239ea129828083d6
Hayk Ghukasyan <76152476+GhukasyanHayk@users.noreply.github.com>
2021-03-25 18:08:13 +0400

    -

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,7 @@ SECRET_KEY = '(qrna3q=#dqpwlg&d_z*93=&s52b&mvs1%^4sdzkuob5igsd_a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 ALLOWED_HOSTS = ['estelarestoran.herokuapp.com','localhost','127.0.0.1']
 
 
