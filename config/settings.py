d90550f70f10db8f0c92a9d81f6af27940e47c68
Hayk Ghukasyan <76152476+GhukasyanHayk@users.noreply.github.com>
2021-03-25 17:28:12 +0400

    -

--- config/settings.py
+++ config/settings.py
@@ -109,10 +109,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "estelarestoran@gmail.com"
